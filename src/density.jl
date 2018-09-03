df152346cb6344aea13967bcb08ccacd41fe3a32
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-09-03 22:20:00 +0100

    implement density (#3)

--- src/density.jl
+++ src/density.jl
@@ -1,7 +1,26 @@
-convert_arguments(P::Type{<: AbstractPlot}, d::UnivariateKDE) =
-    convert_arguments(P, d.x, d.density)
-convert_arguments(P::Type{<: AbstractPlot}, d::BivariateKDE) =
-    convert_arguments(P, d.x, d.y, d.density)
+_to_values(d::UnivariateKDE) = (d.x, d.density)
+_to_values(d::BivariateKDE) = (d.x, d.y, d.density)
+
+convert_arguments(P::Type{<: AbstractPlot}, d::KernelDensity.AbstractKDE) =
+    convert_arguments(P, _to_values(d)...)
 
 plottype(::UnivariateKDE) = Lines
 plottype(::BivariateKDE) = Heatmap
+
+@recipe(Density) do scene
+    Theme()
+end
+
+function AbstractPlotting.plot!(scene::Scene, ::Type{Density}, attributes::Attributes, p...)
+    attr = copy(attributes)
+    hist_kwarg = [:boundary, :npoints, :kernel, :bandwidth]
+    hist_attr = [pop!(attr, key, Signal(nothing)) for key in hist_kwarg]
+    h = lift(hist_attr...) do v...
+        kwargs = (t for t in zip(hist_kwarg, v) if last(t) !== nothing)
+        kde(p...; kwargs...)
+    end
+    _args = lift(_to_values, h)
+    args = Tuple(lift(t->t[i], _args) for i in 1:length(to_value(_args)))
+    plot!(scene, plottype(to_value(h)), attr, args...)
+    scene
+end
--- src/histogram.jl
+++ src/histogram.jl
@@ -1,37 +1,29 @@
-function hist2values(h::StatsBase.Histogram)
+function _to_values(h::StatsBase.Histogram)
     f(edges) = edges[1:end-1] .+ diff(edges)./2
     (map(f, h.edges)..., h.weights)
 end
 
 convert_arguments(P::Type{<:AbstractPlot}, h::StatsBase.Histogram) =
-    convert_arguments(P, hist2values(h)...)
+    convert_arguments(P, _to_values(h)...)
 
 plottype(::StatsBase.Histogram{<:Any, 1}) = BarPlot
 plottype(::StatsBase.Histogram{<:Any, 2}) = Heatmap
+plottype(::StatsBase.Histogram{<:Any, 3}) = Contour
 
 @recipe(Histogram) do scene
-    Theme(;
-        default_theme(scene)...,
-        nbins = nothing,
-        closed = :default_left
-    )
+    Theme()
 end
 
-# From the StatPlots examples in Makie
-
-function AbstractPlotting.plot!(p::Histogram)
-    # we assume that the y kwarg is set with the data to be binned, and nbins is also defined
-    l = length(p.converted)
-    h = lift(p[1:l]..., p[:nbins], p[:closed]) do v...
-        args, nbins, closed =  v[1:end-2], v[end-1], v[end]
-        kwargs = (nbins = nbins, closed = closed)
-        fit(StatsBase.Histogram, args...; Iterators.filter(t -> last(t) !== nothing, pairs(kwargs))...)
+function AbstractPlotting.plot!(scene::Scene, ::Type{Histogram}, attributes::Attributes, p...)
+    attr = copy(attributes)
+    hist_kwarg = [:nbins, :closed]
+    hist_attr = [pop!(attr, key, Signal(nothing)) for key in hist_kwarg]
+    h = lift(hist_attr...) do v...
+        kwargs = (t for t in zip(hist_kwarg, v) if last(t) !== nothing)
+        fit(StatsBase.Histogram, p...; kwargs...)
     end
-    _args = lift(hist2values, h)
+    _args = lift(_to_values, h)
     args = Tuple(lift(t->t[i], _args) for i in 1:length(to_value(_args)))
-    t = copy(theme(p))
-    for (key, val) in t
-        t[key] = get(p, key, val)
-    end
-    plot!(plottype(to_value(h)), t, args...)
+    plot!(scene, plottype(to_value(h)), attr, args...)
+    scene
 end
