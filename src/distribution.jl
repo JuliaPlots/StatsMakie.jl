3a209f874976e778c167b42cce159d7c078acfa3
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-09-04 15:39:43 +0100

    plot qqpair

--- src/distribution.jl
+++ src/distribution.jl
@@ -14,41 +14,42 @@ convert_arguments(P::Type{<:AbstractPlot}, dist::Distribution, args...) = conver
 plottype(::Distribution, args...) = Lines
 plottype(::Distribution{<:VariateForm, <:Discrete}, args...) = ScatterLines
 #-----------------------------------------------------------------------------
-# qqplots
-# 
-# function plot!(P::Type{<:AbstractPlot}, )
-#
-# function plot!(h::QQPair; qqline = :identity)
-#     if qqline in (:fit, :quantile, :identity, :R)
-#         xs = [extrema(h.qx)...]
-#         if qqline == :identity
-#             ys = xs
-#         elseif qqline == :fit
-#             itc, slp = linreg(h.qx, h.qy)
-#             ys = slp .* xs .+ itc
-#         else # if qqline == :quantile || qqline == :R
-#             quantx, quanty = quantile(h.qx, [0.25, 0.75]), quantile(h.qy, [0.25, 0.75])
-#             slp = diff(quanty) ./ diff(quantx)
-#             ys = quanty .+ slp .* (xs .- quantx)
-#         end
-#
-#         @series begin
-#             primary := false
-#             seriestype := :path
-#             xs, ys
-#         end
-#     end
-#
-#     seriestype --> :scatter
-#     legend --> false
-#     h.qx, h.qy
-# end
-#
-# loc(D::Type{T}, x) where T<:Distribution = fit(D, x), x
-# loc(D, x) = D, x
-#
-# @userplot QQPlot
-# @recipe f(h::QQPlot) = qqbuild(loc(h.args[1], h.args[2])...)
-#
-# @userplot QQNorm
-# @recipe f(h::QQNorm) = QQPlot((Normal, h.args[1]))
+# qqplots (M. K. Borregaard implementation from StatPlots)
+
+@recipe(QQNorm) do scene
+    Theme()
+end
+
+@recipe(QQPlot) do scene
+    Theme()
+end
+
+plot!(scene::SceneLike, ::Type{<:QQNorm}, attributes::Attributes, p) = plot!(scene, QQPlot, attributes, Normal, p)
+plot!(scene::SceneLike, ::Type{<:QQPlot}, attributes::Attributes, p...) = plot!(scene, attributes, qqbuild(loc(p...)...))
+
+# function default
+plottype(::QQPair) = Scatter
+
+function plot!(scene::SceneLike, P::Type{<:AbstractPlot}, attributes::Attributes, h::QQPair)
+    attr = copy(attributes)
+    qqline = pop!(attr, :qqline, Signal(:identity)) |> to_value
+    if qqline in (:fit, :quantile, :identity, :R)
+        xs = [extrema(h.qx)...]
+        if qqline == :identity
+            ys = xs
+        elseif qqline == :fit
+            itc, slp = hcat(fill!(similar(h.qx), 1), h.qx) \ h.qy
+            ys = slp .* xs .+ itc
+        else # if qqline == :quantile || qqline == :R
+            quantx, quanty = quantile(h.qx, [0.25, 0.75]), quantile(h.qy, [0.25, 0.75])
+            slp = diff(quanty) ./ diff(quantx)
+            ys = quanty .+ slp .* (xs .- quantx)
+        end
+        plot!(scene, LineSegments, attr, xs, ys)
+    end
+    plot!(scene, P, attr, h.qx, h.qy)
+    scene
+end
+
+loc(D::Type{T}, x) where T<:Distribution = fit(D, x), x
+loc(D, x) = D, x
