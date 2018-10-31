85103dce33ab128bca07cfaf2d004456c8b20371
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-10-31 14:11:30 +0100

    wip

--- src/group.jl
+++ src/group.jl
@@ -8,17 +8,25 @@ function to_function(p::Pair{<:Any, <:AbstractArray})
     t -> last(p)[value2index[t]]
 end
 
-function plot!(scene::SceneLike, ::Type{T}, attributes::Attributes, g::NTuple{<:Any, Pair}, p...) where {T<:AbstractPlot}
+struct Grouped{N<:NTuple{<:Any, Pair}}
+    pairs::N
+end
+
+Grouped(args...) = Grouped(args)
+
+Base.pairs(g::Grouped) = g.pairs
+
+function plot!(p::Combined{Any, Tuple{Grouped, Vararg{N}}}) where {N}
+    g = p[1] |> to_value |> pairs
+    keys(p) |> println
     names = Tuple(a for (a, b) in g)
     cols = Tuple(_first(b) for (a, b) in g)
     funcs = Tuple(to_function(b) for (a, b) in g)
-    coltable = table(cols..., p...; names = [names..., (Symbol("x$i") for i in 1:length(p))...])
+    coltable = table(cols..., to_value.(p[2:N])...; names = [names..., (Symbol("x$i") for i in 1:N)...])
     groupby(coltable, names, usekey = true) do key, dd
-        attr = copy(attributes)
         for (i, (k, v)) in enumerate(pairs(key))
-            attr[k] = Signal(funcs[i](v))
-            plot!(scene, T, attr, columns(dd)...)
+            p[k] = lift(funcs[i], v)
         end
+        scatter!(p, columns(dd)...)
     end
-    scene
 end
