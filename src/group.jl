eaabb0c786908e1b39d2613080fc7886f4be6a65
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-11-03 19:36:34 +0000

    split attributes if of correct length

--- src/group.jl
+++ src/group.jl
@@ -23,17 +23,28 @@ Group(; kwargs...) = Group(kwargs...)
 
 Base.pairs(g::Group) = g.pairs
 
+_split(v, len, idxs) = v
+_split(v::AbstractVector, len, idxs) = length(v) == len ? view(v, idxs) : v
+_typ(::AbstractVector) = AbstractVector
+_typ(::T) where {T} = T
+
 function plot!(p::Combined{T, <: Tuple{Group, Vararg{<:Any, N}}}) where {T, N}
     g = p[1] |> to_value |> pairs
     names = Tuple(a for (a, b) in g)
     cols = Tuple(_first(b) for (a, b) in g)
+    len = length(cols[1])
+
     funcs = Tuple(to_function(b) for (a, b) in g)
-    coltable = table(cols..., to_value.(p[2:(N+1)])...; names = [names..., (Symbol("x$i") for i in 1:N)...])
+    coltable = table(1:len, cols..., to_value.(p[2:(N+1)])...; names = [:row, names..., (Symbol("x$i") for i in 1:N)...])
     groupby(coltable, names, usekey = true) do key, dd
         attr = copy(Theme(p))
+        idxs = column(dd, :row)
+        for (key, val) in attr
+            attr[key] = lift(t -> _split(t, len, idxs), val, typ = _typ(val[]))
+        end
         for (i, (k, v)) in enumerate(pairs(key))
             attr[k] = lift(funcs[i], to_node(v))
         end
-        plot!(p, Combined{T}, attr, columns(dd)...)
+        plot!(p, Combined{T}, attr, columns(dd, Not(:row))...)
     end
 end
