25df25b5aa13b62364ebf50f328ff3e3923bc001
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-11-07 16:44:15 +0000

    remove df test

--- test/runtests.jl
+++ test/runtests.jl
@@ -41,10 +41,3 @@ using Test
 
     @test plts[3][1][] == poly
 end
-
-# @testset "df" begin
-#     t = table(1:10, 2:2:20, names = [:x, :y])
-#     plt = @df t scatter(:x, :y)
-#     @test columns(t, :x) == plt[:x]
-#     @test columns(t, :y) == plt[:y]
-# end
