7bae5d50e2ed060c5bcc3e345f0d732f61c46da4
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-11-15 15:47:30 +0000

    convert to float for volume case

--- src/histogram.jl
+++ src/histogram.jl
@@ -5,7 +5,7 @@ const histogram_plot_types = [BarPlot, Heatmap, Volume]
 function convert_arguments(P::Type{<:AbstractPlot}, h::StatsBase.Histogram{<:Any, N}) where N
     ptype = plottype(P, histogram_plot_types[N])
     f(edges) = edges[1:end-1] .+ diff(edges)./2
-    ptype => convert_arguments(ptype, map(f, h.edges)..., h.weights)
+    ptype => convert_arguments(ptype, map(f, h.edges)..., Float64.(h.weights))
 end
 
 histogram(args...; kwargs...) = fit(StatsBase.Histogram, args...; kwargs...)
