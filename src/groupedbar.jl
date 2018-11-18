0a67163e3af6a34e0f9bf99705dd0f97d8da62b0
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-11-18 13:52:12 +0000

    define in terms of convert_arguments

--- src/groupedbar.jl
+++ src/groupedbar.jl
@@ -1,22 +1,15 @@
-@recipe(GroupedBar) do scene
-    Theme(;
-        default_theme(scene, BarPlot)...
-    )
-end
+@enum BarPosition superimpose dodge stack
 
-function plot!(p::GroupedBar)
-    x, y = p[1:2]
-    attr = copy(Theme(p))
-    mb = get(p, :width, Node(nothing))
-    barwidth = mb[] === nothing ? lift(minimum∘diff, x) : mb
-    n = size(y[], 2)
-    w = lift(t -> 0.8*t/n, barwidth)
-    attr[:width] = w
-    ys = (lift(t -> t[:, i], y) for i in 1:n)
+used_attributes(P::PlotFunc, p::BarPosition, args...) = (:width, :position)
 
-    xs = (lift((t, u) -> t .+ i*u .- u*(n+1)/2, x, w) for i in 1:n)
+function convert_arguments(P::PlotFunc, p::BarPosition, x::AbstractArray, y::AbstractArray; width = automatic)
+    barwidth = width === automatic ? minimum(diff(unique(sort(x)))) : width
+    n = size(y, 2)
+    w = 0.8*barwidth/n
+    adapt(theme) = merge(theme, Theme(width = w))
 
-    for (x′, y′) in zip(xs, ys)
-        plot!(p, BarPlot, attr, x′, y′)
-    end
+    xs = (x .+ i*w .- w*(n+1)/2 for i in 1:n)
+    ys = (y[:, i] for i in 1:n)
+
+    convert_arguments(P, PlotList(collect(zip(xs, ys)); transform_attributes = adapt))
 end
