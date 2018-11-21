f379e38d82a3a2c1a7fcf7dad17283ec5a8a7c3d
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-11-21 19:38:52 +0000

    switch histogram to plotspec

--- src/histogram.jl
+++ src/histogram.jl
@@ -5,7 +5,8 @@ const histogram_plot_types = [BarPlot, Heatmap, Volume]
 function convert_arguments(P::Type{<:AbstractPlot}, h::StatsBase.Histogram{<:Any, N}) where N
     ptype = plottype(P, histogram_plot_types[N])
     f(edges) = edges[1:end-1] .+ diff(edges)./2
-    ptype => convert_arguments(ptype, map(f, h.edges)..., Float64.(h.weights))
+    kwargs = N == 1 ? (; width = step(h.edges[1])) : (;)
+    to_plotspec(ptype, convert_arguments(ptype, map(f, h.edges)..., Float64.(h.weights)); kwargs...)
 end
 
 histogram(args...; kwargs...) = fit(StatsBase.Histogram, args...; kwargs...)
