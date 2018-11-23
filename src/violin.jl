c5a3551c5b58236dcbaa65160fa1893270900646
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-11-23 14:24:30 +0000

    add violin

--- src/violin.jl
+++ src/violin.jl
@@ -1,26 +1,33 @@
-@recipe(Violin) do scene
+@recipe(Violin, x, y) do scene
     Theme(;
         default_theme(scene, Poly)...,
-        side = :both
+        side = :both,
+        width = automatic
     )
 end
 
 function plot!(plot::Violin)
     width, side = plot[:width], plot[:side]
 
-    bigmesh = lift(plot[1], plot[2], width) do x, y, bw
+    bigmesh = lift(plot[1], plot[2], width, side) do x, y, bw, vside
         t = table((x = x, y = y), copy = false, presorted = true)
         gt = groupby(kde, t, :x, select = :y)
+        bw === automatic && (bw = minimum(diff(column(gt, :x))))
         meshes = GeometryTypes.GLPlainMesh[]
         for row in rows(gt)
             min, max = extrema_nan(row.kde.density)
-            x = row.x .+ row.kde.density .* (bw/max)
-            y = row.kde.x
-            mesh = GeometryTypes.GLPlainMesh.(Point2f0.(x, y))
+            xl = reverse(row.x .- row.kde.density .* (0.4*bw/max))
+            xr = row.x .+ row.kde.density .* (0.4*bw/max)
+            yl = reverse(row.kde.x)
+            yr = row.kde.x
+
+            x = vside == :left ? xl : vside == :right ? xr : vcat(xr, xl)
+            y = vside == :left ? yl : vside == :right ? yr : vcat(yr, yl)
+            mesh = GeometryTypes.GLPlainMesh(Point2f0.(x, y))
             push!(meshes, mesh)
         end
-        return merge(meshes...)
+        return merge(meshes)
     end
 
-    mesh!(bigmesh)
+    mesh!(plot, bigmesh)
 end
