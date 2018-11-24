a415f72dd5cc4c49f00347a130fd8bfb6a49cd3e
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-11-24 15:55:50 +0000

    use poly for violin

--- src/violin.jl
+++ src/violin.jl
@@ -15,7 +15,7 @@ function plot!(plot::Violin)
         t = table((x = x, y = y), copy = false, presorted = true)
         gt = groupby(v -> (kde = kde(v), median = median(v)), t, :x, select = :y)
         bw === automatic && (bw = minimum(diff(column(gt, :x))))
-        meshes = GeometryTypes.GLPlainMesh[]
+        polys = Vector{Point2f0}[]
         lines = Pair{Point2f0, Point2f0}[]
         for row in rows(gt)
             min, max = extrema_nan(row.kde.density)
@@ -26,14 +26,13 @@ function plot!(plot::Violin)
 
             x = vside == :left ? xl : vside == :right ? xr : vcat(xr, xl)
             y = vside == :left ? yl : vside == :right ? yr : vcat(yr, yl)
-            mesh = GeometryTypes.GLPlainMesh(Point2f0.(x, y))
-            push!(meshes, mesh)
+            push!(polys, Point2f0.(x, y))
             median_left = Point2f0(vside == :right ? row.x : row.x-(0.4*bw), row.median)
             median_right = Point2f0(vside == :left ? row.x : row.x+(0.4*bw), row.median)
             push!(lines, median_left => median_right)
         end
-        return merge(meshes), lines
+        return polys, lines
     end
-    mesh!(plot, lift(first, signals), color = plot[:color], visible = plot[:visible])
+    ploy!(plot, lift(first, signals), color = plot[:color], visible = plot[:visible])
     linesegments!(plot, lift(last, signals), color = plot[:linecolor], visible = plot[:show_median])
 end
