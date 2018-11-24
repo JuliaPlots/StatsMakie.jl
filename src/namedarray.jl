b458a9f82b18b7ded4fc232e76617d2d701885d2
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-11-24 19:00:13 +0000

    support multidimensional case

--- src/namedarray.jl
+++ src/namedarray.jl
@@ -4,6 +4,6 @@ function convert_arguments(P::PlotFunc, m::NamedArray)
     n = length(axes(m))
     ptype = plottype(P, named_array_plot_types[n])
     args = (names(m, i) for i in 1:n)
-    v = convert(Vector, m)
+    v = convert(Array, m)
     to_plotspec(ptype, convert_arguments(ptype, args..., v))
 end
