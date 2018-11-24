b0b2aa42530f564d07502d139eed610c73bc5d4b
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-11-24 13:38:03 +0000

    licence

--- src/scales.jl
+++ src/scales.jl
@@ -1,3 +1,9 @@
+#=
+Color scheme taken from https://github.com/mkborregaard/MakieThemes.jl
+The MakieThemes.jl package is licensed under the GNU Public License:
+    Copyright (c) 2018: Michael Krabbe Borregaard.
+=#
+
 const default_scales = Dict(
     :color => ["#9b3950", "#f79b57", "#6b3241", "#e65e62", "#9e7587", "#4C8659", "#6B6C69", "#0b0e0a"],
     :marker => [:circle, :xcross, :utriangle, :diamond, :dtriangle, :star6, :pentagon, :rect],
