8037089d40e5695f5cfd3bb87f34efa31900b9d6
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-11-24 13:51:58 +0000

    change star6

--- src/scales.jl
+++ src/scales.jl
@@ -6,7 +6,7 @@ The MakieThemes.jl package is licensed under the GNU Public License:
 
 const default_scales = Dict(
     :color => ["#9b3950", "#f79b57", "#6b3241", "#e65e62", "#9e7587", "#4C8659", "#6B6C69", "#0b0e0a"],
-    :marker => [:circle, :xcross, :utriangle, :diamond, :dtriangle, :star6, :pentagon, :rect],
+    :marker => [:circle, :xcross, :utriangle, :diamond, :dtriangle, :star8, :pentagon, :rect],
     :linestyle => [nothing, :dash, :dot, :dashdot, :dashdotdot],
     :side => [:left, :right]
 )
