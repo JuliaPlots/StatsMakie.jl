8d57857a0412e27f227c0d560647a0c4d16f876d
Seth Axen <seth.axen@gmail.com>
2020-03-18 14:25:05 -0700

    Support constant/vector types and add whiskers

--- src/recipes/errorbar.jl
+++ src/recipes/errorbar.jl
@@ -8,7 +8,7 @@ and vertical error bars (along the `y`-axis)
 on those points, as defined by `Δy`.
 """
 @recipe(ErrorBarY, x, y, Δy) do scene
-    default_theme(scene, LineSegments)
+    Theme(; default_theme(scene, LineSegments)..., whisker_width = 0)
 end
 
 """
@@ -19,7 +19,7 @@ and horizontal error bars (along the `x`-axis)
 on those points, as defined by `Δx`.
 """
 @recipe(ErrorBarX, x, y, Δx) do scene
-    default_theme(scene, LineSegments)
+    Theme(; default_theme(scene, LineSegments)..., whisker_width = 0)
 end
 
 """
@@ -33,23 +33,44 @@ on those points, as defined by `Δx` and `Δy`.
 @recipe(ErrorBar, x, y, Δx, Δy) do scene
     Theme(;
         default_theme(scene, LineSegments)...,
+        whisker_width = 0,
         xcolor = automatic,
-        ycolor = automatic
+        xwhisker_width = automatic,
+        ycolor = automatic,
+        ywhisker_width = automatic,
     )
 end
 
 function plot!(plot::ErrorBarY)
-    segments = map(plot[1], plot[2], plot[3]) do x,y,Δy
-        [Point2f0(x[i], y[i]-Δy[i])=>Point2f0(x[i], y[i]+Δy[i]) for i in 1:length(x)]
+    t = Theme(plot)
+    ww = pop!(t, :whisker_width)
+    segments = lift(plot[1], plot[2], plot[3], ww) do x,y,Δy,ww
+        bar = Pair.(Point2f0.(x, y .- Δy), Point2f0.(x, y .+ Δy))
+        segments = [bar;]
+        if ww != 0
+            lw = Pair.(first.(bar) .- Point2f0.(ww, 0), first.(bar) .+ Point2f0.(ww, 0))
+            uw = Pair.(last.(bar) .- Point2f0.(ww, 0), last.(bar) .+ Point2f0.(ww, 0))
+            append!(segments, [lw; uw])
+        end
+        return segments
     end
-    linesegments!(plot, Theme(plot), segments)
+    linesegments!(plot, t, segments)
 end
 
 function plot!(plot::ErrorBarX)
-    segments = map(plot[1], plot[2], plot[3]) do x,y,Δx
-        [Point2f0(x[i]-Δx[i], y[i])=>Point2f0(x[i]+Δx[i], y[i]) for i in 1:length(x)]
+    t = Theme(plot)
+    ww = pop!(t, :whisker_width)
+    segments = lift(plot[1], plot[2], plot[3], ww) do x,y,Δx,ww
+        bar = Pair.(Point2f0.(x .- Δx, y), Point2f0.(x .+ Δx, y))
+        segments = [bar;]
+        if ww != 0
+            lw = Pair.(first.(bar) .- Point2f0.(0, ww), first.(bar) .+ Point2f0.(0, ww))
+            uw = Pair.(last.(bar) .- Point2f0.(0, ww), last.(bar) .+ Point2f0.(0, ww))
+            append!(segments, [lw; uw])
+        end
+        return segments
     end
-    linesegments!(plot, Theme(plot), segments)
+    linesegments!(plot, t, segments)
 end
 
 function plot!(plot::ErrorBar)
@@ -57,14 +78,17 @@ function plot!(plot::ErrorBar)
 
     t = copy(Theme(plot))
     xcolor, ycolor = pop!.(Ref(t), (:xcolor, :ycolor))
+    xww, yww = pop!.(Ref(t), (:xwhisker_width, :ywhisker_width))
     xcolor = lift((xc,c) -> xc === automatic ? c : xc, xcolor, plot[:color])
     ycolor = lift((yc,c) -> yc === automatic ? c : yc, ycolor, plot[:color])
+    xww = lift((xww,ww) -> xww === automatic ? ww : xww, xww, plot[:whisker_width])
+    yww = lift((yww,ww) -> yww === automatic ? ww : yww, yww, plot[:whisker_width])
 
     #x-error
-    tx = merge(Theme(color = xcolor), t)
+    tx = merge(Theme(color = xcolor, whisker_width = xww), t)
     errorbarx!(plot, tx, x, y, Δx)
     #y-error
-    ty = merge(Theme(color = ycolor), t)
+    ty = merge(Theme(color = ycolor, whisker_width = yww), t)
     errorbary!(plot, ty, x, y, Δy)
     plot
 end
