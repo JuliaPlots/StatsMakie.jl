7735546743f1b4836c2f5928913991d9cd1d9f44
Seth Axen <seth.axen@gmail.com>
2020-03-20 14:06:57 -0700

    Updates to docstring

--- src/recipes/crossbar.jl
+++ src/recipes/crossbar.jl
@@ -2,20 +2,22 @@
     crossbar(x, y, ymin, ymax; kwargs...)
 
 Draw a crossbar. A crossbar represents a range with a (potentially notched) box.
-It is most commonly used as part of the boxplot.
+It is most commonly used as part of the `boxplot`.
 
 # Arguments
-- `x`: Position of the box
-- `y`: Position of the midline within the box
-- `ymin`: Lower limit of the box
-- `ymax`: Upper limit of the box
+- `x`: position of the box
+- `y`: position of the midline within the box
+- `ymin`: lower limit of the box
+- `ymax`: upper limit of the box
 
 # Keywords
 - `orientation=:vertical`: orientation of box (`:vertical` or `:horizontal`)
 - `width=0.8`: width of the box
-- `notch=false`: draw the notch
-- `notchmin=automatic`: Lower limit of the notch
-- `notchmax=automatic`: Upper limit of the notch
+- `show_notch=false`: draw the notch
+- `notchmin=automatic`: lower limit of the notch
+- `notchmax=automatic`: upper limit of the notch
+- `notchwidth=0.5`: multiplier of `width` for narrowest width of notch
+- `show_midline=true`: show midline
 """
 @recipe(CrossBar, x, y, ymin, ymax) do scene
     t = Theme(
